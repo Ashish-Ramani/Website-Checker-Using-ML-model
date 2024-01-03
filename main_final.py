8ab7a5e6cdc507719cab55e898657cff5a08bd7b
Ashish-Ramani <115503513+Ashish-Ramani@users.noreply.github.com>
2024-01-03 15:31:27 -0600

    Update main_final.py

---
+++ main_final.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import time
+import datetime
+
+text = "test.txt"
+
+with open(text, 'r') as file:
+    html_data = file.read()
+    abc = html_data.split()
+    ret = []
+    for i in abc:
+        if i.find('https:') == 0 or i.find('http:') == 0:
+            ret.append(i)
+    for url_https in ret:
+        url = "https://www.virustotal.com/api/v3/urls"
+        
+        payload = { "url": f"{url_https}" }
+        headers = {
+            "accept": "application/json",
+            "x-apikey": "a5dfadc88d5a7aef293d8759ee3e540dc6ef410c6a1017ffdf11f2c9617a88f4",
+            "content-type": "application/x-www-form-urlencoded"
+        }
+
+        response1 = requests.post(url, data=payload, headers=headers)
+        data = response1.json()
+        analysis_id = data["data"]["id"]
+        id_parts = analysis_id.split("-")
+        id = id_parts[1]
+        url_id=(url+"/"+id)
+        headers1 = {
+            "accept": "application/json",
+            "x-apikey": "a5dfadc88d5a7aef293d8759ee3e540dc6ef410c6a1017ffdf11f2c9617a88f4"
+        }
+        response = requests.get(url_id, headers=headers1)
+        url_vote=(url+"/"+id+"/votes?limit=10")
+        headers = {
+            "accept": "application/json",
+            "x-apikey": "a5dfadc88d5a7aef293d8759ee3e540dc6ef410c6a1017ffdf11f2c9617a88f4"
+        }
+
+        response_vote = requests.get(url_vote, headers=headers)
+        data1=response.json()
+        last_analysis_stats = data1["data"]["attributes"]["last_analysis_stats"]
+        print(last_analysis_stats)
+        value = f"['{url_https}':{last_analysis_stats}], \n"
+        print(datetime.datetime.now())
+        time.sleep(15)
+        with open('data.txt','a') as file:
+            file.write(str(value))
